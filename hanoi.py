d8acd25c081cd49d77a187e2221db6fb8c58ab0f
itsepid <sepidetajik@yahoo.com>
2024-10-15 14:31:14 +0330

    another commit

---
+++ hanoi.py
@@ -0,0 +1,13 @@
+def main():
+    x=int(input('enter a number'))
+    A='A'
+    B='B'
+    C='C'
+    print(Hanoi(x,A,B,C))
+
+def Hanoi(n, A,B,C):
+    if n==1:
+        return f'{A}-{C}'
+    else:
+        return f'{Hanoi(n-1,A,C,B)},{A}-{C},{Hanoi(n-1,B,A,C)}'
+main()
